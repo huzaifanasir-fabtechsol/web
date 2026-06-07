520752386b3df8a1e0584853f09921d74f95587d
HuzaifaNasirFab <huzaifanasirfab@fabtechsol.com>
2026-06-07 08:38:00 +0500

    Update config.js

--- forntend/web/src/api/config.js
+++ forntend/web/src/api/config.js
@@ -1,2 +1,2 @@
 const devApiUrl = "http://localhost:8000";
-export const API_URL = import.meta.env.VITE_API_BASE_URL || import.meta.env.VITE_API_URL || (import.meta.env.DEV ? devApiUrl : "");
+export const API_URL = import.meta.env.DEV ? devApiUrl : "";
